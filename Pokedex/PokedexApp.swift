56290cbbe0827d7fbc627e4fa29777e7084995c1
Vinícius Couto Tasso <vcoutasso@gmail.com>
2023-01-07 21:57:45 -0300

    chore: create xcode project

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Vinícius Couto on 07/01/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
